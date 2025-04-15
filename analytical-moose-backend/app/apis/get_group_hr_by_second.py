d88459fc242ecbb8e1997af9049bcca27611c6de
david-der <dder@singlestoneconsulting.com>
2025-04-15 12:53:32 -0400

    working checkpoint with frontend

--- analytical-moose-backend/app/apis/get_group_hr_by_second.py
+++ analytical-moose-backend/app/apis/get_group_hr_by_second.py
@@ -4,13 +4,12 @@ from app.functions.aggregated_per_second import aggregateHeartRateSummaryPerSeco
 from datetime import datetime, timedelta
 
 class QueryParams(BaseModel):
-    user_name: str
     timestamp: str #Pydantic will parse the str to a datetime object
 
 class QueryResult(BaseModel):
     user_name: str
-    rounded_up_time: int
-    avg_hr_per_second: float
+    last_processed_timestamp: int
+    avg_heart_rate: float
 
 def get_hr_api(client: MooseClient, params: QueryParams) -> QueryResult:
     dt = datetime.fromisoformat(params.timestamp.replace('Z', '+00:00'))
@@ -18,6 +17,7 @@ def get_hr_api(client: MooseClient, params: QueryParams) -> QueryResult:
     rounded_dt = rounded_dt - timedelta(seconds=1)
     formatted_timestamp = rounded_dt.strftime("%Y-%m-%dT%H:%M:%S")
     timestamp = formatted_timestamp
+    # timestamp = params.timestamp
 
     query = f"""
         SELECT
@@ -31,8 +31,8 @@ def get_hr_api(client: MooseClient, params: QueryParams) -> QueryResult:
                 max(processed_timestamp) AS last_processed_timestamp,
                 rounded_up_time,
                 avgMerge(avg_hr_per_second) AS avg_heart_rate
-            FROM {aggregateHeartRateSummaryPerSecondMV.target_table.name}
-            WHERE last_processed_timestamp >= toDateTime({timestamp})
+            FROM per_second_heart_rate_aggregate_view
+            WHERE processed_timestamp >= toDateTime('{timestamp}')
             GROUP BY user_name, rounded_up_time
             ORDER BY user_name, rounded_up_time
         )
