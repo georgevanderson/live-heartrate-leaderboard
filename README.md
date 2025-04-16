17410755bbea21fa4f2de9e94f206460e20f9408
david-der <dder@singlestoneconsulting.com>
2025-04-16 13:26:47 -0400

    stable commit two days prior

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 ## SOS 
 
 ```docker stop $(docker ps -aq)```
+
+```bash -i <(curl -fsSL https://fiveonefour.com/install.sh) moose, aurora```
---
+++ analytical-moose-backend/app/index.ts
@@ -0,0 +1,6 @@
+
+
+export * from './apis/__init__';
+export * from './apis/getLatestUserHeartRates';
+export * from './apis/getUserHeartRateStats';
+export * from './apis/get_group_hr_by_second';
